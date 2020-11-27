b2ab236cfdfe58d2c8f3392158b8b5369345145c
Bojan Pantovic <bojan.pantovic@gmail.com>
2020-11-27 12:05:59 +0100

    Dodato citanje javascript fajla

--- js/function.js
+++ js/function.js
@@ -58,36 +58,8 @@ function mojRezultat(){
 	}
 }
 
-$(document).ready(function () { 
-
-    // FETCHING DATA FROM JSON FILE
-
-    $.getJSON("kursna_lista.json",  
-            function (data) { 
-        var student = ''; 
-
-        // ITERATING THROUGH OBJECTS 
-        $.each(data, function (key, value) { 
-
-            //CONSTRUCTION OF ROWS HAVING 
-            // DATA FROM JSON OBJECT 
-            student += '<tr>'; 
-            student += '<td>' +  
-                value.GFGUserName + '</td>'; 
-
-            student += '<td>' +  
-                value.NoOfProblems + '</td>'; 
-
-            student += '<td>' +  
-                value.TotalScore + '</td>'; 
-
-            student += '<td>' +  
-                value.Articles + '</td>'; 
-
-            student += '</tr>'; 
-        }); 
-          
-        //INSERTING ROWS INTO TABLE  
-        $('#table').append(student); 
-    }); 
-}); 
+$(document).ready(function () {
+	var mydata = JSON.parse(data);
+	console.log(mydata);
+	console.log(mydata[0]);
+});
