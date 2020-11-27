f15c9fe5dc834c93d54f17bd7aefc9eea0e3ff79
unknown <bojan.trivanovic@limolabs.com>
2020-11-27 11:26:33 +0100

    Menjacnica prva verzija

--- js/function.js
+++ js/function.js
@@ -58,8 +58,36 @@ function mojRezultat(){
 	}
 }
 
-$(document).ready(function() {
-    $('#example').DataTable( {
-        "ajax": 'http://www.json-generator.com/api/json/get/cjnIrxkIUi?indent=2'
-    } );
-} );
+$(document).ready(function () { 
+
+    // FETCHING DATA FROM JSON FILE
+
+    $.getJSON("kursna_lista.json",  
+            function (data) { 
+        var student = ''; 
+
+        // ITERATING THROUGH OBJECTS 
+        $.each(data, function (key, value) { 
+
+            //CONSTRUCTION OF ROWS HAVING 
+            // DATA FROM JSON OBJECT 
+            student += '<tr>'; 
+            student += '<td>' +  
+                value.GFGUserName + '</td>'; 
+
+            student += '<td>' +  
+                value.NoOfProblems + '</td>'; 
+
+            student += '<td>' +  
+                value.TotalScore + '</td>'; 
+
+            student += '<td>' +  
+                value.Articles + '</td>'; 
+
+            student += '</tr>'; 
+        }); 
+          
+        //INSERTING ROWS INTO TABLE  
+        $('#table').append(student); 
+    }); 
+}); 
